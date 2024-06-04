a8e1486aa96f142d44d58e4b194ceb9d1bf2cedf
Martin Massera <martinmassera@gmail.com>
2024-06-04 12:08:10 -0300

    commented retrieve name

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,10 +1,18 @@
 from marto_python.admin import register_admin
 from .models import Chat
 from django.contrib.admin import ModelAdmin
+# from asgiref.sync import async_to_sync
+# from . import bot_updates
 
 
 class ChatAdmin(ModelAdmin):
     list_display = ['pk', 'chat_id', 'username', 'is_group', 'send_lesson']
+    # actions = ['retrieve_name']
+    #
+    # def retrieve_name(self, request, queryset):
+    #     async_to_sync(bot_updates.initialize_bot)()  # TODO: find better way to do this, so we don't have to initialize in every action
+    #     for chat in queryset.all():
+    #         async_to_sync(bot_updates.retrieve_chat_name)(chat)
 
 
 register_admin(Chat, ChatAdmin)
