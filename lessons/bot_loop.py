c635c49fbcfe5a656ee5f89530b5a2e465514e7c
Martin Massera <martinmassera@gmail.com>
2026-05-04 14:35:24 -0300

    polling_error_callback to see connection errors as warnings

--- lessons/bot_loop.py
+++ lessons/bot_loop.py
@@ -1,5 +1,6 @@
 import logging
 import asyncio
+from telegram.error import NetworkError, TelegramError
 from lessons.bot_updates import initialize_bot
 from lessons.bot import try_send_all
 
@@ -8,9 +9,16 @@ logger = logging.getLogger(__name__)
 
 async def run_bot_loop():
     application = await initialize_bot()
-    await application.updater.start_polling()
+    await application.updater.start_polling(error_callback=polling_error_callback)
     await application.start()
     await send_all_loop()
+
+
+def polling_error_callback(error: TelegramError):
+    if isinstance(error, NetworkError):
+        logger.warning(f'Telegram polling network error: {error}')
+    else:
+        logger.exception('Exception happened while polling for updates.')
 
 
 async def send_all_loop():
